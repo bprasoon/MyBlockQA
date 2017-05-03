5e815efc62b1e1c73e4b0f62fa8ee3cf39893426
x136980 <x136980@BCPRD46S8TAG-L.hrbinc.hrblock.net>
2017-05-03 11:47:16 -0500

    configuration file URL change.

---
+++ src/main/java/Objectrepository/App.java
@@ -0,0 +1,17 @@
+package Objectrepository;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\x136980\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        ChromeDriver driver= new ChromeDriver();
+    }
+}
