f7c78c1d5cc64c86af469eae03095e5a5088c3a1
x136980 <x136980@BCPRD46S8TAG-L.hrbinc.hrblock.net>
2017-05-03 14:06:51 -0500

    Organized the Import files -- Cntl+Shift+O

--- src/test/java/Testcases/Profile_Page.java
+++ src/test/java/Testcases/Profile_Page.java
@@ -2,14 +2,10 @@ package Testcases;
 
 import java.awt.AWTException;
 import java.awt.Robot;
-import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
